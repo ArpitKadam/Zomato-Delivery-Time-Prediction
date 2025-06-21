a5f260c1ab4583ec327e90345d306bceebfea22e
Arpit Kadam <arpitkadam922@gmail.com>
2025-06-21 06:15:18 +0530

    setup file and src file commit

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirements = []
+    with open('requirements.txt') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+
+    return requirements
 
 setup(
     name = "zomato_delivery_time_prediction",
@@ -6,5 +18,6 @@ setup(
     author = "ArpitKadam",
     author_email="arpitkadam922@gmail.com",
     description="End to End Zomato Delivery Time Prediction Project using MLOps",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires = get_requirements(),
 )
